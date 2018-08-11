ce0daf034cdc74d35017f3e2b830621e1c9da48f
ku5za <kuszkuszewski@gmail.com>
2018-08-11 11:34:03 +0200

    Added:	favicon 		better form validation and reseting chosen login

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,10 @@ const info = document.querySelector("#login-exclamation");
 const login = document.querySelector("#login");
 const password = document.querySelector("#password");
 const submit = document.querySelector("#submit");
+const resetButton = document.querySelector("#reset");
+const chosenLogin = document.querySelector("#chosen-login");
+const clearLogin = document.querySelector("#clear-login");
+
 
 function isNumber() {
     const loginValue = parseInt(login.value, 10);
@@ -11,13 +15,22 @@ function isNumber() {
         // In some cases user can try put a coma(,) or dot(.) sign in our login input
         // (input type="number" allows this), what can produce a bug, so we need to check
         // if there is any fraction in our number
-        info.style.display = "block";
+        
+        info.style.visibility = "visible";
         info.textContent = "Login może zawierać tylko cyfry";
         login.style.border = "1px solid #F90000";
+        if(login.value.length === 0){
+            info.textContent = "Login nie może być pusty";
+            login.style.border = "1px solid #F90000";
+            info.style.visibility = "visible";
+
+            return 0;
+        }
 
         return 0;
-    } else {
-        info.style.display = "none";
+    } 
+    else {
+        info.style.visibility = "hidden";
         login.style.border = "none";
 
         return 1;
@@ -31,35 +44,48 @@ function loginValidation() {
         if(loginValue.length !== 9){
             info.textContent = "Login musi zawierać 9 cyfr";
             login.style.border = "1px solid #F90000";
-            info.style.display = "block";
-        } else {
-            const chosenLogin = document.querySelector("#chosen-login");
-            chosenLogin.textContent = login.value;
-            chosenLogin.style.display = "block";
-            login.style.display = "none";
-            info.style.display = "none";
-            password.style.display = "block";
+            info.style.visibility = "visible";
         }
-    };
+        else {
+            chosenLogin.textContent = login.value;
+            chosenLogin.style.visibility = "visible";
+            clearLogin.style.display = "inline-block";
+            password.style.visibility = "visible";
+            info.style.visibility = "hidden";
+            login.style.display = "none";
+        }
+    }
 }
 
 function passwordValidation() {
     if(password.value.length === 0) {
         password.style.border = "1px solid #F90000";
-        info.style.display = "block";
+        info.style.visibility = "visible";
         info.textContent = "Hasło nie może być puste";
         submit.disabled = true;
-    } else {
-        info.style.display = "none";
+    }
+    else {
+        info.style.visibility = "hidden";
         password.style.border = "none";
         submit.disabled = false;
     }
 }
 
+function resetLogin() {
+    password.style.border = "none";
+    password.style.visibility = "hidden";
+    password.value = "";
+    submit.disabled = true;
+    login.value = "";
+    login.style.display = "inline-block";
+    chosenLogin.textContent = "";
+    chosenLogin.style.visibility = "hidden";
+    clearLogin.style.display = "none";
+}
+
+
 login.addEventListener("change", loginValidation);
 login.addEventListener("keyup", isNumber);
 password.addEventListener("keyup", passwordValidation);
-
-(function(){
-    console.log('Witamy na Campie 2018! ');
-})();
+resetButton.addEventListener("click", resetLogin);
+clearLogin.addEventListener("click", resetLogin);
